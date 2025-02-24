a70ad1d995893024e5f952704f0cb8dfc5623bad
ortizal19009 <ort19009@byui.edu>
2025-02-24 18:53:07 -0500

    update datos

---
+++ comercializacion/src/test/java/com/erp/comercializacion/ComercializacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.comercializacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercializacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
