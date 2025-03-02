c6b24b578de39881bb26308b635ff997760b5ee6
ortizal19009 <ort19009@byui.edu>
2025-03-01 20:21:04 -0500

    update datos

--- config/src/main/java/com/erp/config/ConfigApplication.java
+++ config/src/main/java/com/erp/config/ConfigApplication.java
@@ -3,9 +3,8 @@ package com.erp.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigApplication {
 
 	public static void main(String[] args) {
