9319c6c3a2ab775f16eebf65e0701344dcfd6f3e
ortizal19009 <ort19009@byui.edu>
2025-03-04 22:52:28 -0500

    update datos

---
+++ login/src/main/java/com/erp/login/models/Usuarios.java
@@ -0,0 +1,37 @@
+package com.erp.login.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "usuarios")
+public class Usuarios {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idusuario;
+    private String identificausu;
+    private String cosusu;
+    private String nomusu;
+    private String email;
+    private Boolean estado;
+    private LocalDate fdesde;
+    private LocalDate fhasta;
+    private ZonedDateTime feccrea;
+    private Long usumodi;
+    private ZonedDateTime fecmodi;
+    private Boolean otrapestania;
+    private String alias;
+    private String priusu;
+    private String perfil;
+    private Long toolbarframe;
+    private Long toolbarsheet;
+}
