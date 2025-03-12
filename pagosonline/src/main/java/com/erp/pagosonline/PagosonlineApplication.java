ca79e50f39f346f96835fc08448cb57b30d1ebbf
ortizal19009 <ort19009@byui.edu>
2025-03-11 22:07:22 -0500

    update datos

---
+++ pagosonline/src/main/java/com/erp/pagosonline/PagosonlineApplication.java
@@ -0,0 +1,13 @@
+package com.erp.pagosonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosonlineApplication.class, args);
+	}
+
+}
