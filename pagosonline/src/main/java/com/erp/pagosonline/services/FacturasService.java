83803dee0de6448010a377d62f34187a0a04845d
ortizal19009 <ort19009@byui.edu>
2025-03-26 23:23:53 -0500

    update microservicios conections

--- pagosonline/src/main/java/com/erp/pagosonline/services/FacturasService.java
+++ pagosonline/src/main/java/com/erp/pagosonline/services/FacturasService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
 import java.net.StandardSocketOptions;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,22 +19,24 @@ public class FacturasService {
     @Autowired
     private RestTemplate rt;
     private List<Long> idfacturas;
-    private final String URL_INTERESES = "http:localhost:8080/intereses";
+    private final String URL_INTERESES = "http://localhost:9093/intereses";
 
     public FacturaDTO findFacturasSinCobro(Long cuenta) {
         // Obtener la lista de facturas desde el DAO
         List<FacturasSinCobroInter> facturas = dao.findFacturasSinCobro(cuenta);
         FacturaDTO facturaDTO = new FacturaDTO(); // Crear el DTO
         List<Long> idfacturas = new ArrayList<>(); // Inicializar lista de IDs
-
+        BigDecimal total = BigDecimal.ZERO;
         // Llenar la lista con los IDs de las facturas
         facturas.forEach(item -> {
             System.out.println(item.getIdfactura());
-
+            System.out.println(getInteres(item));
+            total.add(item.getSubtotal());
             idfacturas.add(item.getIdfactura()); // Agregar el ID a la lista
         });
         facturaDTO.setResponsablepago(facturas.get(0).getNombre());
         facturaDTO.setCuenta(cuenta);
+        facturaDTO.setTotal(total);//CORREGIR EL VALOR TOTAL DE LA FACTURA
         // Asignar la lista al DTO
         facturaDTO.setFacturas(idfacturas);
 
@@ -41,7 +44,7 @@ public class FacturasService {
         return facturaDTO;
     }
     public Object getInteres(FacturasSinCobroInter factura){
-        Object _factura = rt.getForObject(this.URL_INTERESES+"/calcularInteres", Object.class);
+        Object _factura = rt.getForObject(this.URL_INTERESES+"/calcularInteres?formapago="+factura.getFormapago()+"&subtotal="+factura.getSubtotal()+"&feccrea="+factura.getFeccrea(), Object.class);
         return _factura;
     }
 
