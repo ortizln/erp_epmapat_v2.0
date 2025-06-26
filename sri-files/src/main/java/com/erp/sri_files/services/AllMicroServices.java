c65d1245925c9a605cce66bf434e2a624c319a8c
ortizln <ortizln9@gmail.com>
2025-06-26 15:43:57 -0500

    update restTemplate

--- sri-files/src/main/java/com/erp/sri_files/services/AllMicroServices.java
+++ sri-files/src/main/java/com/erp/sri_files/services/AllMicroServices.java
@@ -1,5 +1,6 @@
 package com.erp.sri_files.services;
 
+import com.erp.sri_files.config.AppConfig;
 import com.erp.sri_files.interfaces.fecFacturaDatos;
 import com.erp.sri_files.models.Factura;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +9,18 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AllMicroServices {
+    //@Autowired
+    //private RestTemplate restTemplate;
     @Autowired
-    private RestTemplate restTemplate;
+    private AppConfig restTemplate;
     private final String URL_COMERCIALIZACION_FACTURAS = "http://192.168.0.69:8080/facturas";
     private final String URL_COMERCIALIZACION_FEC_FACTURAS = "http://192.168.0.69:8080/fec_factura";
     public Factura findById(Long idFactura){
-        Factura factura = restTemplate.getForObject(URL_COMERCIALIZACION_FACTURAS+"/"+idFactura, Factura.class);
+        Factura factura = restTemplate.restTemplate().getForObject(URL_COMERCIALIZACION_FACTURAS+"/"+idFactura, Factura.class);
         return  factura;
     }
     public fecFacturaDatos getNroFactura(Long idfactura){
-        fecFacturaDatos fec_factura = restTemplate.getForObject(URL_COMERCIALIZACION_FACTURAS+"/fecFacturaDatos"+idfactura, fecFacturaDatos.class);
+        fecFacturaDatos fec_factura = restTemplate.restTemplate().getForObject(URL_COMERCIALIZACION_FEC_FACTURAS+"/fecFacturaDatos"+idfactura, fecFacturaDatos.class);
         return  fec_factura;
     }
 }
--- sri-files/src/test/java/com/erp/sri_files/SriFilesApplicationTests.java
+++ sri-files/src/test/java/com/erp/sri_files/SriFilesApplicationTests.java
@@ -4,7 +4,10 @@ package com.erp.sri_files;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"eureka.client.enabled=false",
+		"spring.cloud.discovery.enabled=false"
+})
 class SriFilesApplicationTests {
 
 	@Test
