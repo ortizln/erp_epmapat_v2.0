33a83618dc0d08a54599a5cd04e71f7c8a895aca
ortizln <ortizln9@gmail.com>
2025-06-26 16:24:44 -0500

    Update sh

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Lista de microservicios (carpetas)
+modules=("config" "sri-files" "comercializacion" "eureka" "gestiondocumental" "login" "pagosonline" "recaudacion" "sri-files")
 
 for module in "${modules[@]}"
 do
