ccfe1790c63803a07eed80b7ef27aff6243f6dce
ortizal19009 <ort19009@byui.edu>
2025-07-02 17:49:18 -0500

    Update variables de entorno

--- reportes-jr/src/main/java/com/erp/reportes_jr/ReportesJrApplication.java
+++ reportes-jr/src/main/java/com/erp/reportes_jr/ReportesJrApplication.java
@@ -2,7 +2,10 @@ package com.erp.reportes_jr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +13,10 @@ public class ReportesJrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportesJrApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
--- sri-files/src/main/java/com/erp/sri_files/SriFilesApplication.java
+++ sri-files/src/main/java/com/erp/sri_files/SriFilesApplication.java
@@ -2,12 +2,21 @@ package com.erp.sri_files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SriFilesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SriFilesApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
