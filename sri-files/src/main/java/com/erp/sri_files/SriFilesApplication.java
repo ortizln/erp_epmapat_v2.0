37705ed60e82e00bec2a985527a59fa643fc0d33
ortizal19009 <ort19009@byui.edu>
2025-07-02 17:50:43 -0500

    Update variables de entorno

--- sri-files/src/main/java/com/erp/sri_files/SriFilesApplication.java
+++ sri-files/src/main/java/com/erp/sri_files/SriFilesApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +16,7 @@ public class SriFilesApplication {
 		SpringApplication.run(SriFilesApplication.class, args);
 	}
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
