ae5c4a520fd977072a2f2d9973b654971b25a7d7
ortizln <ortizln9@gmail.com>
2025-07-04 11:15:37 -0500

    update microservice sri

--- sri-files/src/main/java/com/erp/sri_files/interfaces/fecFacturaDatos.java
+++ sri-files/src/main/java/com/erp/sri_files/interfaces/fecFacturaDatos.java
@@ -3,12 +3,12 @@ package com.erp.sri_files.interfaces;
 import lombok.*;
 
 import java.time.LocalDate;
-@Data
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class fecFacturaDatos {
-    private Long id;
-    private String numeroFactura;
+    private Long idfactura;
+    private String xmlautorizado;
+    private LocalDate fechaemision;
 }
