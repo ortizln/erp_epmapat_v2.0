8013f65d5e98d8e4e60a0afaf10a420388547ee2
ortizln <ortizln9@gmail.com>
2025-09-01 16:30:11 -0500

    Update comercializacion

---
+++ comercializacion/src/main/java/com/erp/comercializacion/controllers/UsuariosApi.java
@@ -0,0 +1,113 @@
+package com.erp.comercializacion.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.erp.comercializacion.excepciones.ResourceNotFoundExcepciones;
+import com.erp.comercializacion.interfaces.UsuarioI;
+import com.erp.comercializacion.models.Usuarios;
+import com.erp.comercializacion.services.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*")
+
+public class UsuariosApi {
+
+   @Autowired
+   UsuarioServicio usuServicio;
+
+   @GetMapping
+   public List<Usuarios> getAll() {
+      return usuServicio.findAll();
+   }
+
+   @GetMapping("/usuario")
+   public Usuarios getUsuario(@Param(value = "a") String a, @Param(value = "b") String b) {
+      if (a != null && b != null) {
+         return usuServicio.findUsuario(a, b);
+      } else {
+         return null;
+      }
+   }
+
+   // Va a servir para validar
+   @GetMapping("/identificacion")
+   public Usuarios getByIdentificausu(@Param(value = "identificausu") String identificausu) {
+      if (identificausu != null) {
+         return usuServicio.findByIdentificausu(identificausu);
+      } else {
+         return null;
+      }
+   }
+
+   @GetMapping("/{idusuario}")
+   public ResponseEntity<Usuarios> getByIdusuario(@PathVariable Long idusuario) {
+      Usuarios usuario = usuServicio.findById(idusuario)
+            .orElseThrow(() -> new ResourceNotFoundExcepciones(
+                  ("No existe el Usuario: " + idusuario)));
+      return ResponseEntity.ok(usuario);
+   }
+
+   @PutMapping("/{idusuario}")
+   public ResponseEntity<Usuarios> update(@PathVariable Long idusuario, @RequestBody Usuarios x) {
+      Usuarios y = usuServicio.findById(idusuario)
+            .orElseThrow(() -> new ResourceNotFoundExcepciones(
+                  ("No existe Usuario con Id: " + idusuario)));
+      y.setIdentificausu(x.getIdentificausu());
+      y.setNomusu(x.getNomusu());
+      y.setCodusu(x.getCodusu());
+      y.setFdesde(x.getFdesde());
+      y.setFhasta(x.getFhasta());
+      y.setEstado(x.getEstado());
+      y.setEmail(x.getEmail());
+      y.setFeccrea(x.getFeccrea());
+      y.setUsumodi(x.getUsumodi());
+      y.setFecmodi(x.getFecmodi());
+      y.setOtrapestania(x.getOtrapestania());
+      y.setAlias(x.getAlias());
+      y.setPriusu(x.getPriusu());
+      y.setPerfil(x.getPerfil());
+      y.setToolbarframe(x.getToolbarframe());
+      y.setToolbarsheet(x.getToolbarsheet());
+
+      Usuarios actualizar = usuServicio.save(y);
+      return ResponseEntity.ok(actualizar);
+   }
+
+   @PostMapping
+   public ResponseEntity<Object> saveUsuario(@RequestBody Usuarios user) {
+      Map<String, Object> response = new HashMap<>();
+      Usuarios _user= usuServicio.save(user);
+      if(_user != null ){
+         response.put("status", ResponseEntity.ok());
+         response.put("message", "Usuario creado");
+         
+      }else{
+         response.put("status", ResponseEntity.ok());
+         response.put("message", "Usuario no creado");
+      }
+      return ResponseEntity.ok(response);
+
+   }
+
+   @GetMapping("one")
+   public ResponseEntity<UsuarioI> findDatosById(@RequestParam Long idusuario) {
+      UsuarioI usuario = usuServicio.findDatosById(idusuario);
+      return ResponseEntity.ok(usuario);
+   }
+}
---
+++ comercializacion/src/main/java/com/erp/comercializacion/models/Usuarios.java
@@ -0,0 +1,175 @@
+package com.erp.comercializacion.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "usuarios")
+
+public class Usuarios {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long idusuario;
+   private String identificausu;
+   private String codusu;
+   private String nomusu;
+   private String email;
+   private Boolean estado;
+   @Column(name = "fdesde")
+   private LocalDate fdesde;
+   @Column(name = "fhasta")
+   private LocalDate fhasta;
+   @Column(name = "feccrea")
+   private ZonedDateTime feccrea;
+   private Long usumodi;
+   @Column(name = "fecmodi")
+   private ZonedDateTime fecmodi;
+   private Boolean otrapestania;
+   private String alias;
+   private String priusu;
+   private String perfil;
+   private Long toolbarframe; 
+   private Long toolbarsheet;
+
+   // ====== Getteres y Setters =======
+   public String getPerfil() {
+      return perfil;
+   }
+
+   public void setPerfil(String perfil) {
+      this.perfil = perfil;
+   }
+
+   public String getPriusu() {
+      return priusu;
+   }
+
+   public void setPriusu(String priusu) {
+      this.priusu = priusu;
+   }
+
+   public LocalDate getFdesde() {
+      return fdesde;
+   }
+
+   public void setFdesde(LocalDate fdesde) {
+      this.fdesde = fdesde;
+   }
+
+   public LocalDate getFhasta() {
+      return fhasta;
+   }
+
+   public void setFhasta(LocalDate fhasta) {
+      this.fhasta = fhasta;
+   }
+
+   public Long getIdusuario() {
+      return idusuario;
+   }
+
+   public void setIdusuario(Long idusuario) {
+      this.idusuario = idusuario;
+   }
+
+   public String getIdentificausu() {
+      return identificausu;
+   }
+
+   public void setIdentificausu(String identificausu) {
+      this.identificausu = identificausu;
+   }
+
+   public String getCodusu() {
+      return codusu;
+   }
+
+   public void setCodusu(String codusu) {
+      this.codusu = codusu;
+   }
+
+   public String getNomusu() {
+      return nomusu;
+   }
+
+   public void setNomusu(String nomusu) {
+      this.nomusu = nomusu;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public Boolean getEstado() {
+      return estado;
+   }
+
+   public void setEstado(Boolean estado) {
+      this.estado = estado;
+   }
+
+   public ZonedDateTime getFeccrea() {
+      return feccrea;
+   }
+
+   public void setFeccrea(ZonedDateTime feccrea) {
+      this.feccrea = feccrea;
+   }
+
+   public Long getUsumodi() {
+      return usumodi;
+   }
+
+   public void setUsumodi(Long usumodi) {
+      this.usumodi = usumodi;
+   }
+
+   public ZonedDateTime getFecmodi() {
+      return fecmodi;
+   }
+
+   public void setFecmodi(ZonedDateTime fecmodi) {
+      this.fecmodi = fecmodi;
+   }
+
+   public Boolean getOtrapestania() {
+      return otrapestania;
+   }
+
+   public void setOtrapestania(Boolean otrapestania) {
+      this.otrapestania = otrapestania;
+   }
+
+   public String getAlias() {
+      return alias;
+   }
+
+   public void setAlias(String alias) {
+      this.alias = alias;
+   }
+
+   public Long getToolbarframe() {
+      return toolbarframe;
+   }
+
+   public void setToolbarframe(Long toolbarframe) {
+      this.toolbarframe = toolbarframe;
+   }
+
+   public Long getToolbarsheet() {
+      return toolbarsheet;
+   }
+
+   public void setToolbarsheet(Long toolbarsheet) {
+      this.toolbarsheet = toolbarsheet;
+   }
+   
+
+}
