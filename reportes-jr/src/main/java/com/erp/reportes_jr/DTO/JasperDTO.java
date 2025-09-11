c54d4bc7ab147239b605a58cdfd85a0f75ebc731
ortizal19009 <ort19009@byui.edu>
2025-09-11 16:29:10 -0500

    update reportesjr

--- reportes-jr/src/main/java/com/erp/reportes_jr/DTO/JasperDTO.java
+++ reportes-jr/src/main/java/com/erp/reportes_jr/DTO/JasperDTO.java
@@ -1,35 +1,16 @@
 package com.erp.reportes_jr.DTO;
-import lombok.Data;
+import lombok.*;
 
 import java.util.HashMap;
 import java.util.Map;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class JasperDTO {
     private String reportName;
     private Map<String, Object> parameters = new HashMap<>();
     private String extencion;
 
-    public String getReportName() {
-        return reportName;
-    }
-
-    public void setReportName(String reportName) {
-        this.reportName = reportName;
-    }
-
-    public Map<String, Object> getParameters() {
-        return parameters;
-    }
-
-    public void setParameters(Map<String, Object> parameters) {
-        this.parameters = parameters;
-    }
-
-    public String getExtencion() {
-        return extencion;
-    }
-
-    public void setExtencion(String extencion) {
-        this.extencion = extencion;
-    }
 }
