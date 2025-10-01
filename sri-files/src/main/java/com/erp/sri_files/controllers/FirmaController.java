341035b1a373eb81a944d84e0f47acde9f3c9201
ortizal <alexis.ortiz81@outlook.com>
2025-10-01 16:23:54 -0500

    envio de facturaas electronicas exitoso

--- sri-files/src/main/java/com/erp/sri_files/controllers/FirmaController.java
+++ sri-files/src/main/java/com/erp/sri_files/controllers/FirmaController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/firmas")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class FirmaController {
 
     private final EnvioComprobantesWs envioComprobantesWs;
