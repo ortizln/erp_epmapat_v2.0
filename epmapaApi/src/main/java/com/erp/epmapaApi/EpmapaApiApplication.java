c4dd184948c29dcfe867d0e6bd5e2f991511b647
ortizal <alexis.ortiz81@outlook.com>
2025-11-28 12:00:31 -0500

    Create api epmapat

---
+++ epmapaApi/src/main/java/com/erp/epmapaApi/EpmapaApiApplication.java
@@ -0,0 +1,13 @@
+package com.erp.epmapaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpmapaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpmapaApiApplication.class, args);
+	}
+
+}
