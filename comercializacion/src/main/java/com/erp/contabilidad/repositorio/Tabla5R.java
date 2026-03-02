204702fc13eb000e7dcafd78f00e68deaa62d26a
ortizal19009 <ort19009@byui.edu>
2026-03-01 22:11:22 -0500

    refactor(comercializacion): desacoplar módulos rrhh y contabilidad del microservicio

--- comercializacion/src/main/java/com/erp/contabilidad/repositorio/Tabla5R.java
+++
@@ -1,9 +0,0 @@
-package com.erp.contabilidad.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.erp.contabilidad.modelo.Tabla5;
-
-public interface Tabla5R extends JpaRepository<Tabla5, Long> {
-    
-}
-
