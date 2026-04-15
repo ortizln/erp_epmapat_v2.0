65e0294eed49ec6db17a721d21e4bddedd1fe7cc
ortizal19009 <ort19009@byui.edu>
2026-04-14 19:40:48 -0500

    update

--- emails/src/main/java/com/erp/emails/EmailsApplication.java
+++ emails/src/main/java/com/erp/emails/EmailsApplication.java
@@ -2,8 +2,10 @@ package com.erp.emails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EmailsApplication {
 
 	public static void main(String[] args) {
