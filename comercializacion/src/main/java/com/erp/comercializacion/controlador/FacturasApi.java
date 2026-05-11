abfc83b8d2f042d5f330fef1c9e238752af96c17
ortizln <ortizln9@gmail.com>
2026-05-11 11:58:40 -0500

    create msvc-bandred

--- comercializacion/src/main/java/com/erp/comercializacion/controlador/FacturasApi.java
+++ comercializacion/src/main/java/com/erp/comercializacion/controlador/FacturasApi.java
@@ -21,7 +21,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
